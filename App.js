3551f4e6726a49803235c2fb25273b4276b7d832
Edward Ha <admin@MacBook-Pro-cua-Admin.local>
2023-08-31 21:38:44 +0700

    Add navigation

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, TouchableOpacity, View } from 'react-native';
 import tw from 'twrnc'
+import Navigation from './navigation';
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-black">
-      <TouchableOpacity className="bg-teal-500 rounded-lg p-3 shadow-md shadow-gray-400">
-        <Text className="text-white text-3xl font-bold">Hello world</Text>
-      </TouchableOpacity>
-      <StatusBar style="light" />
-    </View>
+    <Navigation />
   );
 }
