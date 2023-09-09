04db0aa7dc5c63c7a8addcce6a2ad7fb4bd223bc
Edward Ha <admin@MacBook-Pro-cua-Admin.local>
2023-09-09 16:41:00 +0700

    Feature add animated package

---
+++ src/components/layout/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Drawler } from 'react-native';
+const Drawler = () => {
+  return (
+    <Drawler /> 
+  )
+}
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,18 +2,56 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import Login from "../screens/Login";
+import Setting from "../screens/Setting";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import { BeakerIcon } from '@heroicons/react/solid'
+import { Text, TouchableOpacity, View } from "react-native";
+import HeaderIcon from "../components/layout/HeaderIcon";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+// const CustomDrawerContent = (props) => {
+//   return (
+//     <DrawerContentScrollView {...props}>
+//       <DrawerItemList {...props} />
+//       <DrawerItem
+//         label="Close drawer"
+//         onPress={() => props.navigation.closeDrawer()}
+//       />
+//       <DrawerItem
+//         label="Toggle drawer"
+//         onPress={() => props.navigation.toggleDrawer()}
+//       />
+//     </DrawerContentScrollView>
+//   );
+// };
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        screenOptions={({ navigation }) => ({
+          headerLeft: (props) => <HeaderIcon onPress={navigation.toggleDrawer} /> 
+        })}
+        // useLegacyImplementation
+        // drawerContent={(props) => <CustomDrawerContent {...props} />}
+      >
+        <Drawer.Screen name="Home" component={HomeScreen} options={{
+          title: 'Home',
+          headerStyle: {
+            backgroundColor: '#FFF2F2'
+          }
+        }}/>
+        {/* <Drawer.Screen name="Login" component={Login} /> */}
+        <Drawer.Screen name="Setting" component={Setting} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
