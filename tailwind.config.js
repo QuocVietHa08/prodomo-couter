bd4e9dd647d9cd93707971c12a08f71f633d9173
Edward Ha <admin@MacBook-Pro-cua-Admin.local>
2023-09-09 12:06:41 +0700

    Feature update home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/screens/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"], 
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
